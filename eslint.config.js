09535c8e6fe4ba2ef87e8233b17698e286d6f52c
ohgodwhy2k <flamboyance@atomicmail.io>
2025-11-23 02:49:07 -0600

    fix: Update ESLint config to treat Scratch function as existing [dia]

--- eslint.config.js
+++ eslint.config.js
@@ -4,84 +4,85 @@ import jsdoc from "eslint-plugin-jsdoc";
 import prettier from "eslint-config-prettier";
 
 export default [
-    js.configs.recommended,
-    
-    {
-        plugins: {
-            jsdoc,
-        },
-        rules: {
-            ...jsdoc.configs.recommended.rules,
-            "jsdoc/require-returns-description": "off",
-            "jsdoc/require-param-description": "off",
-        },
-    },
+  js.configs.recommended,
 
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.es2021
-            },
-            parserOptions: {
-                ecmaVersion: 2021,
-                sourceType: "module",
-            }
-        },
-        rules: {
-            "quotes": ["error", "single"],
-            "semi": ["error", ";"],
-            "prefer-const": "error",
-        }
+  {
+    plugins: {
+      jsdoc,
     },
-
-    {
-        files: ["src/**/*.js", "src/**/*.mjs"],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            },
-            parserOptions: {
-                sourceType: "module",
-            }
-        },
-        rules: {
-            "jsdoc/require-jsdoc": "off",
-        }
+    rules: {
+      ...jsdoc.configs.recommended.rules,
+      "jsdoc/require-returns-description": "off",
+      "jsdoc/require-param-description": "off",
     },
+  },
 
-    {
-        files: ["docs/**/*.js"],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            },
-            parserOptions: {
-                sourceType: "script", // Often simple, non-module scripts
-            }
-        },
-        rules: {
-            "no-undef": "off",
-        }
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
     },
-
-    {
-        files: ["*.config.js"],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-            parserOptions: {
-                sourceType: "script", 
-            }
-        },
-        rules: {
-            "no-console": "off", // Allow console.log in config files
-            "jsdoc/require-jsdoc": "off",
-        }
+    rules: {
+      quotes: ["error", "single"],
+      semi: ["error", ";"],
+      "prefer-const": "error",
     },
+  },
 
-    prettier,
+  {
+    files: ["src/**/*.js", "src/**/*.mjs"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        Scratch: "readonly",
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "jsdoc/require-jsdoc": "off",
+    },
+  },
+
+  {
+    files: ["docs/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    rules: {
+      "no-undef": "off",
+    },
+  },
+
+  {
+    files: ["*.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "jsdoc/require-jsdoc": "off",
+    },
+  },
+
+  prettier,
 ];
